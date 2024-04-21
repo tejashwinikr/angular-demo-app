d566c339aae01eecb2bb4d36e8e2cc2db5f339b9
tejashwini K R <ahwiniteju@gmail.com>
2024-04-21 20:18:36 +0530

    ngstyle,ngclass and @switch if,else

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Course } from '../model/course';
 import { COURSES } from '../../db-data';
 
@@ -9,23 +9,40 @@ import { COURSES } from '../../db-data';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css',
 })
-export class CourseCardComponent implements OnInit{
+export class CourseCardComponent implements OnInit {
   @Input()
   course!: Course;
 
   @Input({ required: true })
   index!: number;
-  
+
   @Output()
-  courseSelected = new EventEmitter<Course>()
+  courseSelected = new EventEmitter<Course>();
 
-  constructor(){}
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 
   onCourseViewed() {
-    console.log(" course card COMPO - btn clicked")
-    this.courseSelected.emit(this.course)
+    console.log(' course card COMPO - btn clicked');
+    this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    return {
+      beginner: this.course.category == 'BEGINNER',
+    };
+
+    // if (this.course.category == 'BEGINNER') {
+    //   return 'beginner';
+    // }
+    // return null
+  }
+
+  cardStyles() {
+    return {
+      'text-decoration': 'underline',
+      'color':'orange',
+      // 'backgound-image': 'url('+ this.course.iconUrl +')',
+    };
   }
 }
