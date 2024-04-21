639a81b7c01637e05db9f0f0450de76a4982d9e5
tejashwini K R <ahwiniteju@gmail.com>
2024-04-21 12:56:19 +0530

    src from course added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+
+
+
 }
