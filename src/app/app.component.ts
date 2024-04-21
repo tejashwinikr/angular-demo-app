ce036823d529571ea61642d4b5491e7f6811a6a3
tejashwini K R <ahwiniteju@gmail.com>
2024-04-21 20:44:25 +0530

    angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Course } from './model/course';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   courses = COURSES;
+  title = COURSES[0].description;
+  price = 599;
+  rate = 0.67;
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
   // coreCourse = COURSES[0];
   // rxjsCourse = COURSES[1];
   // ngrxCourse = COURSES[2];
