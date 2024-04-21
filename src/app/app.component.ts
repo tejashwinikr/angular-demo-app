bc92b9f3e519f880ed9a26bd521da1ec6d0e22f9
tejashwini K R <ahwiniteju@gmail.com>
2024-04-21 13:39:41 +0530

    displaying data and custom event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
-
-
+  onCourseSelected(course: Course) {
+    console.log(' APP COMPO - btn clicked', course);
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { Course } from '../model/course';
+import { COURSES } from '../../db-data';
 
 @Component({
   selector: 'app-course-card',
   // standalone: true,
   // imports: [],
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  styleUrl: './course-card.component.css',
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit{
+  @Input()
+  course!: Course;
 
+  
+  @Output()
+  courseSelected = new EventEmitter<Course>()
+
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+
+  onCourseViewed() {
+    console.log(" course card COMPO - btn clicked")
+    this.courseSelected.emit(this.course)
+  }
 }
